dc16a22dee0b480e146ce78b3660598598ea1dae
carlos <carlos.faundez14@gmail.com>
2017-06-21 23:51:25 +0000

    Realizando pruebas: Eventos con button y mouse

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,35 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseButton;
 import javafx.stage.Stage;
+import javafx.scene.input.MouseEvent;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Group root = new Group();
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
+        Button button = new Button("Exit");
+        button.setDefaultButton(true);
+        button.setLayoutX(130);button.setLayoutY(125);
+        EventHandler<MouseEvent> eventHandler = new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                System.exit(0);
+            }
+        };
+        button.addEventFilter(MouseEvent.MOUSE_CLICKED,eventHandler);
+        root.getChildren().add(button);
         primaryStage.show();
     }
 
